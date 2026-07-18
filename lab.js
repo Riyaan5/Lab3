73431a5959ef793a98fee8db176e16274f6deab7
Rakib <rakibul.hasan.riyaan@gmail.com>
2026-07-18 20:52:20 +0600

    first commit

---
+++ lab.js
@@ -0,0 +1,49 @@
+let name= "Rakibul Hasan";
+let roll= "29";
+console.log(name);
+console.log(roll);
+let num1=10;
+let num2=20;
+let sum=num1+num2;
+console.log(sum);
+let num=17;
+if(num%2==0){
+    console.log("EVEN");
+}else{
+   console.log("ODD"); 
+}
+let a=25;
+let b=18;
+if(a>b){
+    console.log(a+"is larger");
+}else{
+   console.log(b+"is larger"); 
+}
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+/*
+int sum =0;
+for(let i=1;i<=0;i++){
+    sum=sum+i;
+}
+console.log(sum);
+*/
+let num4=5;
+for(let i=1;i<=10;i++){
+    console.log(num4+"*"+i+"="+(num4*i));
+}
+let n=5;
+let fact=1;
+for(let i=1;i<=n;i++){
+    fact= fact*i;
+}
+console.log(fact);
+
+let c=20;
+let d=10;
+console.log("Addition=" , c+d);
+console.log("Substraction=" , c-d);
+console.log("Multiplication=" , c*d);
+console.log("Division=" , c/d);
+console.log("Modulas=" , c%d);
